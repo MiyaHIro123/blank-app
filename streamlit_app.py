b5b0bc0bef8dff666efa27d07f9c6d303ea81d68
MiyaHIro123 <hiro4900221@gmail.com>
2026-07-07 19:54:27 +0900

    streamlit_app.py を更新

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,49 +2,32 @@ import streamlit as st
 
 st.set_page_config(
     page_title="TUS Archery",
-    layout="wide"
+    layout="wide",
+    initial_sidebar_state="collapsed"  # 最初は閉じた状態
 )
 
 # 初期ページ
 if "page" not in st.session_state:
     st.session_state.page = "ホーム"
 
-# CSS
-st.markdown("""
-<style>
-.title{
-    font-size:32px;
-    font-weight:bold;
-    margin-bottom:20px;
-}
+# 左上タイトル
+st.markdown(
+    "<h1 style='margin-top:0;'>TUS Archery</h1>",
+    unsafe_allow_html=True
+)
 
-div.stButton > button{
-    width:100%;
-    height:45px;
-    font-size:18px;
-}
-</style>
-""", unsafe_allow_html=True)
+# サイドバー
+with st.sidebar:
+    st.title("メニュー")
 
-# タイトル
-st.markdown('<div class="title">TUS Archery</div>', unsafe_allow_html=True)
-
-# ナビゲーション
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    if st.button("射数管理"):
-        st.session_state.page = "射数管理"
-
-with col2:
-    if st.button("ホーム"):
+    if st.button("🏠 ホーム", use_container_width=True):
         st.session_state.page = "ホーム"
 
-with col3:
-    if st.button("カレンダー"):
-        st.session_state.page = "カレンダー"
+    if st.button("🎯 射数管理", use_container_width=True):
+        st.session_state.page = "射数管理"
 
-st.divider()
+    if st.button("📅 カレンダー", use_container_width=True):
+        st.session_state.page = "カレンダー"
 
 # ページ表示
 st.header(st.session_state.page)
