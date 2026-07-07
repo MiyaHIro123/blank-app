c40d27c1a56fdc05d032ff424cc7e6893c817c62
MiyaHIro123 <hiro4900221@gmail.com>
2026-07-07 19:47:09 +0900

    streamlit_app.py を更新

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,41 @@
 import streamlit as st
-from streamlit_navigation_bar import st_navbar
+from streamlit_option_menu import option_menu
 
 st.set_page_config(
     page_title="TUS Archery",
     layout="wide"
 )
 
-# 左上タイトル
+# CSS
 st.markdown("""
 <style>
-div[data-testid="stAppViewContainer"]::before{
-    content:"TUS Archery";
-    position:fixed;
-    top:15px;
-    left:20px;
-    font-size:26px;
-    font-weight:bold;
-    z-index:9999;
+/* 左上タイトル */
+.logo{
+    position: fixed;
+    top: 12px;
+    left: 20px;
+    font-size: 28px;
+    font-weight: bold;
+    z-index: 999;
 }
 
-/* タイトルと重ならないように */
+/* 本文との余白 */
 .block-container{
-    padding-top:4rem;
+    padding-top: 4rem;
 }
 </style>
 """, unsafe_allow_html=True)
 
+# 左上タイトル
+st.markdown('<div class="logo">TUS Archery</div>', unsafe_allow_html=True)
+
 # ナビゲーション
-page = st_navbar(
-    ["射数管理", "ホーム", "カレンダー"],
-    selected="ホーム",
+page = option_menu(
+    menu_title=None,
+    options=["射数管理", "ホーム", "カレンダー"],
+    icons=["bullseye", "house", "calendar"],
+    orientation="horizontal",
+    default_index=1,
 )
 
 # ページ表示
